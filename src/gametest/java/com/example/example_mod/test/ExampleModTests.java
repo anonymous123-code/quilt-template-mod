67a2c5d2ccbf3d1421dfb54f1deb34a78055f2a9
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2023-02-13 19:20:08 +0100

    Update to 1.19.3

--- src/gametest/java/com/example/example_mod/test/ExampleModTests.java
+++ src/gametest/java/com/example/example_mod/test/ExampleModTests.java
@@ -4,11 +4,12 @@ import net.minecraft.block.Blocks;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
+import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
 
-public class ExampleModTests {
+public class ExampleModTests implements QuiltGameTest {
 	@GameTest()
 	public void exampleStructureTest (TestContext context) {
 		context.setBlockState(new BlockPos(0, 2, 0), Blocks.DIAMOND_BLOCK.getDefaultState());
-		context.addInstantFinalTask(() -> context.checkBlock(new BlockPos(0,2,0), block -> block == Blocks.DIAMOND_BLOCK, "Expect block to be a Diamond Block"));
+		context.succeedWhen(() -> context.checkBlock(new BlockPos(0,2,0), block -> block == Blocks.DIAMOND_BLOCK, "Expect block to be a Diamond Block"));
 	}
 }
